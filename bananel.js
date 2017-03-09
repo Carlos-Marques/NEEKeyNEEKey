19423f29c7a1984171d9a50f72bb32d7828eb90d
carlosmarques.personal@gmail.com <carlosmarques.personal@gmail.com>
2017-03-09 18:14:03 +0000

    Bananel added

---
+++ bananel.js
@@ -0,0 +1,18 @@
+var five = require('johnny-five');
+var chipio = require('chip-io');
+
+var board = new five.Board({
+  io: new chipio()
+});
+
+board.on('ready', function() {
+  // Create an LED on the XIO-P0 pin
+  var led = new five.Pin({
+  pin: 71,
+  type: "analog"
+});
+
+pin.read(function(error, value) {
+  console.log(value);
+});
+});
