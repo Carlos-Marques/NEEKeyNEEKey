bcb624610512e88114d30097d7b94a2bbca00050
carlosmarques.personal@gmail.com <carlosmarques.personal@gmail.com>
2017-03-17 14:45:49 +0000

    final

--- core.js
+++ core.js
@@ -1,18 +1,78 @@
 var five = require('johnny-five');
 var chipio = require('chip-io');
+const readline = require('readline');
+const nodemailer = require('nodemailer');
+
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function hidden(query, callback) {
+    var stdin = process.openStdin(),
+        i = 0;
+    process.stdin.on("data", function(char) {
+        char = char + "";
+        switch (char) {
+            case "\n":
+            case "\r":
+            case "\u0004":
+                stdin.pause();
+                break;
+            default:
+                process.stdout.write("\033[2K\033[200D"+query+"["+((i%2==1)?"=-":"-=")+"]");
+                i++;
+                break;
+        }
+    });
+
+    rl.question(query, function(value) {
+        rl.history = rl.history.slice(1);
+        callback(value);
+    });
+}
 
 var board = new five.Board({
   io: new chipio()
 });
 
 board.on('ready', function() {
-  // Create an LED on the XIO-P0 pin
   var led = new five.Pin({
   pin: 71,
   type: "analog"
 });
 
+var on = 0;
+
 pin.read(function(error, value) {
-  console.log(value);
+  if (value > 60 && on == 0) {
+    hidden("password : ", function(password) {
+        // create reusable transporter object using the default SMTP transport
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'carlosmarques.personal@gmail.com',
+                pass: password
+            }
+        });
+
+        // setup email data with unicode symbols
+        let mailOptions = {
+            from: '"Banana Master 🍌" <carlosmarques.personal@gmail.com>', // sender address
+            to: 'isabel.pecastelo@gmail.com', // list of receivers
+            subject: '🍌 Banana Warning 🍌', // Subject line
+            text: 'Your banana has been touched...', // plain text body
+            html: '<b>Your banana has been touched...</b>' // html body
+        };
+
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message %s sent: %s', info.messageId, info.response);
+        });
+    });
+  }
 }); 
 });
